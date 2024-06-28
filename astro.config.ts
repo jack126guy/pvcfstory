113e76d9f115ab61ae7c32734a85eb8719ab0bc6
Jack Grayson <jackgraysonfox@gmail.com>
2024-06-28 13:30:21 -0700

    Check story code in demo API

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import classicJs from '@halfgray/vite-plugin-classic-js';
 
 export default defineConfig({
+	output: 'server',
 	vite: {
 		build: {
 			assetsInlineLimit: 0,
--- src/pages/api/story_exists.ts
+++ src/pages/api/story_exists.ts
@@ -1,5 +1,8 @@
-export function GET() {
-	const responseObj = { doesExist: true };
+import { type APIContext } from 'astro';
+
+export function GET({ request }: APIContext) {
+	const storyCode = new URL(request.url).searchParams.get('code');
+	const responseObj = { doesExist: storyCode === 'EXAMPLE' };
 	return new Response(JSON.stringify(responseObj), {
 		headers: { 'Content-Type': 'application/json' },
 	});
